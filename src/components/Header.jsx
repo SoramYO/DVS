0842df9b2ddc0543fbce68fcebc120ada6032248
ductain <taindse170080@fpt.edu.vn>
2024-05-30 17:01:15 +0700

    header and navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import {
+  DownOutlined,
+  PhoneFilled,
+  TwitchFilled,
+  MailFilled,
+} from "@ant-design/icons";
+import "../css/Header.css";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="headerContainer">
+      <div className="headerLeft">
+        <div className="languageIcon">
+          <img src="assets/language.png" alt="" className="languageIconImage"/>
+        </div>
+          <DownOutlined className="downIcon"/>
+        <h4>English</h4>
+      </div>
+      <div className="headerRight">
+        <TwitchFilled className="headerIcon" />
+        <MailFilled className="headerIcon" />
+        <PhoneFilled className="headerIcon" />
+        <h5>0032-3-233-91-60</h5>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
