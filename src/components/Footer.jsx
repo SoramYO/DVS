71f6901e25da8b61d39b1e69802a73a214eb57cc
sneytaa <thy14903@gmail.com>
2024-05-31 11:40:05 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React from 'react';
+import "../css/Footer.css";
+import AddressIcon from '../assets/icons/map.svg';
+import PhoneIcon from '../assets/icons/phone.svg';
+import EmailIcon from '../assets/icons/mail.svg';
 
 const Footer = () => {
   return (
-    //<div>Footer</div>
     
     <footer className="footer">
-    <div className="contact-information">
-      <h3>Contact information</h3>
-      <address>
-        Antwerpdiamonds.direct<br />
-        Pelikaanstraat 78 - room 2002, 2018 Antwerp, BE<br />
-        0032-3-233-91-60<br />
-        info@antwerpdiamonds.direct
-      </address>
-    </div>
-    <div className="newsletter">
-      <h3>Sign up to not miss any news</h3>
-      <p>Enter your latest offers, news and promotions straight to your inbox. Join our email promotions list and we’ll let you know about the best deals!</p>
-      <button className="subscribe-button">Subscribe</button>
-    </div>
-  </footer>
-  )
+      <div className="contact-information">
+        <h1>Contact information</h1>
+        <address>
+          <div className="contact-item">
+          <img src={EmailIcon} alt="Email Icon" className="icon" />
+            Antwerpdiamonds.direct<br />
+            Pelikaanstraat 78 - room 2002, 2018 Antwerp, BE
+          </div>
+          <div className="contact-item">
+            <img src={PhoneIcon} alt="Phone Icon" className="icon" />
+            0032-3-233-91-60
+          </div>
+          <div className="contact-item">
+          <img src={AddressIcon} alt="Address Icon" className="icon" />
+            info@antwerpdiamonds.direct
+          </div>
+        </address>
+      </div>
+      <div className="newsletter">
+        <h1>Sign up to not miss any news</h1>
+        <p>Enter your latest offers, news and promotions straight to your inbox. Join our email promotions list and we’ll let you know about the best deals!</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
